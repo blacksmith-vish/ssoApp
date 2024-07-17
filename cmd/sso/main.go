02a97cbaa631e356f90d837739d24a62ad4d7f97
vishenosik-lnx-home <n.kuznecov@russianit.ru>
2024-07-18 00:16:05 +0400

    feat: init conf, log, entry point

---
+++ cmd/sso/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+
+	// инициализация конфига
+	conf := config.MustLoad()
+
+	// инициализация логирования
+	log := setupLogger(conf.Env)
+
+	log.Info("start app")
+
+	// TODO: инициализация приложения (app)
+
+	// TODO: инициализация gRPC-сервер
+
+}
+
+func setupLogger(env string) *slog.Logger {
+
+	var log *slog.Logger
+
+	switch env {
+
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ internal/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env string `yaml:"env" env-default:"local"`
+
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+
+	TokenTTL time.Duration `yaml:"token_ttl" env-required:"true"`
+
+	GRPC GRPCConfig `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if len(path) == 0 {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist: " + path)
+	}
+
+	conf := new(Config)
+
+	if err := cleanenv.ReadConfig(path, conf); err != nil {
+		panic("failed to parse config file: " + err.Error())
+	}
+
+	return conf
+}
+
+// flag > env > default
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+
+	flag.Parse()
+
+	if len(res) == 0 {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
