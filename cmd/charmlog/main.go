81fec7f362532ef30038933fe603f1356ed8e080
Vishenosik <nik.kuzya192@mail.ru>
2024-08-18 23:05:10 +0400

    feat(charm log): Подключил библиотеку c красивыми логами. Пока что в отдельном cmd - файле. Провожу тесты, возможно пересяду на него полностью.

---
+++ cmd/charmlog/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+)
+
+type cup int
+
+func (c cup) String() string {
+	s := fmt.Sprintf("%d cup", c)
+	if c > 1 {
+		s += "s"
+	}
+	return s
+}
+
+func startOven(degree int) {
+	log.Helper()
+	log.Debug("Starting oven", "temperature", degree)
+}
+
+func main() {
+
+	lgr := log.New(os.Stdout)
+
+	lgr.SetFormatter(log.JSONFormatter)
+	lgr.SetLevel(log.DebugLevel)
+
+	lgr.Debug("SOME SHIT")
+
+	logger := log.NewWithOptions(os.Stderr, log.Options{
+		ReportCaller:    true,
+		ReportTimestamp: true,
+		TimeFormat:      time.Kitchen,
+		Prefix:          "Baking 🍪 ",
+	})
+
+	// Override the default error level style.
+	styles := log.DefaultStyles()
+	styles.Levels[log.ErrorLevel] = lipgloss.NewStyle().
+		SetString("ERROR!!").
+		Padding(0, 1, 0, 1).
+		Background(lipgloss.Color("204")).
+		Foreground(lipgloss.Color("0"))
+	// Add a custom style for key `err`
+	styles.Keys["err"] = lipgloss.NewStyle().Foreground(lipgloss.Color("204"))
+	styles.Values["err"] = lipgloss.NewStyle().Bold(true)
+	logger = log.New(os.Stderr)
+	logger.SetStyles(styles)
+	logger.Error("Whoops!", "err", "kitchen on fire")
+
+	logger.Info("Starting oven!", "degree", 375)
+	time.Sleep(1 * time.Second)
+	logger.Info("Finished baking")
+
+	log.SetTimeFormat(time.Kitchen)
+	log.SetLevel(log.DebugLevel)
+
+	var (
+		butter    = cup(1)
+		chocolate = cup(2)
+		flour     = cup(3)
+		sugar     = cup(5)
+		temp      = 375
+		bakeTime  = 10
+	)
+
+	startOven(temp)
+	time.Sleep(time.Second)
+	log.Debug("Mixing ingredients", "ingredients",
+		strings.Join([]string{
+			butter.String() + " of butter",
+			chocolate.String() + " of chocolate",
+			flour.String() + " of flour",
+			sugar.String() + " of sugar",
+		}, "\n"),
+	)
+	time.Sleep(time.Second)
+	if sugar > 2 {
+		log.Warn("That's a lot of sugar", "amount", sugar)
+	}
+	log.Info("Baking cookies", "time", fmt.Sprintf("%d minutes", bakeTime))
+	time.Sleep(2 * time.Second)
+	log.Info("Increasing temperature", "amount", 300)
+	temp += 300
+	time.Sleep(time.Second)
+	if temp > 500 {
+		log.Error("Oven is too hot", "temperature", temp)
+		log.Fatal("The kitchen is on fire 🔥")
+	}
+}
--- go.mod
+++ go.mod
@@ -19,18 +19,28 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
