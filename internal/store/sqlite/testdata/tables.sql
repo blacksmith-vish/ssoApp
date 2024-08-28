edeb9d52917b884081b16f59effaefd77b14fe30
Vishenosik <nik.kuzya192@mail.ru>
2024-08-28 19:43:42 +0400

    feat(docker testing): Написал тесты через dockertest.

---
+++ internal/store/sqlite/testdata/tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE examples (
+    id SERIAL PRIMARY KEY,
+    text VARCHAR(200) NOT NULL
+);
+
+INSERT INTO
+    examples (text)
+VALUES
+    ('val1'), ('val2'), ('val3');
+
+SELECT
+    *
+FROM
+    examples;
